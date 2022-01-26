15ae9441408f17d5eaefb3dd3e2d1970aecdb39e
nel-nuss <nussberger@stud.uni-heidelberg.de>
2022-01-26 10:25:52 +0100

    edited data_download.bat to include train stations

--- data_download.bat
+++ data_download.bat
@@ -5,8 +5,8 @@ set /p user_bbox=
 echo Die ausgewaehlte Bounding Box ist %user_bbox%
 
 echo Daten-Zeitstempel eingeben (Format: 2022-12-31; ca. 10 Tage zurueckliegendes Datum empfohlen) : 
-set /p user_time=
-echo Der ausgewaehlte Zeitstempel ist %user_time%
+set /p user_date=
+echo Der ausgewaehlte Zeitstempel ist %user_date%
 
 echo Groesse des Bereitstellungsraums (in Quadratmeter) eingeben : 
 set /p user_area=
@@ -64,4 +64,12 @@ curl -X POST ^
 --data-urlencode "time=%user_date%" ^
 --data-urlencode "filter=(building=sports_hall or building=pavilion or leisure=dance or amenity=community_centre or amenity=exhibition_centre)" ^
 -o shelters.geojson ^
+https://api.ohsome.org/v1/elements/geometry
+
+:: Herunterladen von Bahnhöfen
+curl -X POST ^
+--data-urlencode "bboxes=%user_bbox%" ^
+--data-urlencode "time=%user_date%" ^
+--data-urlencode "filter=railway=station" ^
+-o trainstations.geojson ^
 https://api.ohsome.org/v1/elements/geometry
