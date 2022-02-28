c2d6968bb671e2ee07c32a4ed135631193404a00
nel-nuss <nussberger@stud.uni-heidelberg.de>
2022-02-28 19:16:17 +0100

    added script for proximity rasters and raster calculation (not fully functional)

---
+++ proximity_and_calc.bat
@@ -0,0 +1,53 @@
+:: Script to reproject generate proximity rasters and reclassify accordingly.
+
+:: generate proximity rasters for all layers
+
+:: user area
+gdal_proximity -srcband 1 -distunits GEO -maxdist 500.0 -ot Float32 -of GTiff rast_user_area.tif proxim_user_area.tif
+
+:: elec points
+gdal_proximity -srcband 1 -distunits GEO -maxdist 250.0 -ot Float32 -of GTiff rast_elec.tif proxim_elec.tif
+
+:: sanitary facilities
+gdal_proximity -srcband 1 -distunits GEO -maxdist 250.0 -ot Float32 -of GTiff rast_sani.tif proxim_sani.tif
+
+:: shelters
+gdal_proximity -srcband 1 -distunits GEO -maxdist 250.0 -ot Float32 -of GTiff rast_shelters.tif proxim_shelters.tif
+
+::supermarkets
+gdal_proximity -srcband 1 -distunits GEO -maxdist 1000.0 -ot Float32 -of GTiff rast_super.tif proxim_super.tif
+
+:: train stations
+gdal_proximity -srcband 1 -distunits GEO -maxdist 500.0 -ot Float32 -of GTiff rast_trainstations.tif proxim_trainstations.tif
+
+:: water points
+gdal_proximity -srcband 1 -distunits GEO -maxdist 250.0 -ot Float32 -of GTiff rast_water_point.tif proxim_water_point.tif
+
+
+
+:: reclassify to be able to calculate
+
+:: user area
+gdal_calc -A proxim_user_area.tif --outfile=reclass_proxim_user_area.tif --calc="100*(A<=100)+50*(A>100)*(A<=500)+10*(A>500)"
+
+:: elec points
+gdal_calc -A proxim_elec.tif --outfile=reclass_proxim_elec.tif --calc="100*(A<=100)+50*(A>100)*(A<=250)+10*(A>250)"
+
+:: sanitary facilites
+gdal_calc -A proxim_sani.tif --outfile=reclass_proxim_sani.tif --calc="100*(A<=100)+50*(A>100)*(A<=250)+10*(A>250)"
+
+:: shelters
+gdal_calc -A proxim_shelters.tif --outfile=reclass_proxim_shelters.tif --calc="100*(A<=100)+50*(A>100)*(A<=250)+10*(A>250)"
+
+:: supermarkets
+gdal_calc -A proxim_super.tif --outfile=reclass_proxim_super.tif --calc="100*(A<=500)+50*(A>500)*(A<=1000)+10*(A>1000)"
+
+:: train stations
+gdal_calc -A proxim_trainstations.tif --outfile=reclass_proxim_trainstations.tif --calc="100*(A<=100)+50*(A>100)*(A<=500)+10*(A>500)"
+
+:: water points
+gdal_calc -A proxim_water_point.tif --outfile=reclass_proxim_water_point.tif --calc="100*(A<=100)+50*(A>100)*(A<=250)+10*(A>250)"
+
+
+:: calculate values
+gdal_calc -A reclass_proxim_user_area.tif -B reclass_proxim_elec.tif -C reclass_proxim_sani.tif -D reclass_proxim_shelters.tif -E reclass_proxim_super.tif -F reclass_proxim_trainstations.tif -G reclass_proxim_water_point.tif -- calc="A+B+C+D+E+F+G"
