541cf774796ebde1e5f220d6ca28882679f6497a
nel-nuss <nussberger@stud.uni-heidelberg.de>
2022-03-09 14:06:13 +0100

    made CRS a user variable

--- reproject_rasterize.bat
+++ reproject_rasterize.bat
@@ -1,25 +1,32 @@
 :: Script to reproject the downloaded vector layers using ogr2ogr and rasterizing them with gdal_rasterize (GDAL)
 
+@echo OFF
+
+echo Enter EPSG code : 
+set /p user_crs=
+echo The CRS has been set to EPSG %user_crs%
+
+
 :: user_area 
-ogr2ogr -t_srs EPSG:32632 reproj_user_area.geojson user_area.geojson
+ogr2ogr -t_srs EPSG:%user_crs% reproj_user_area.geojson user_area.geojson
 
 :: water_point 
-ogr2ogr -t_srs EPSG:32632 reproj_water_point.geojson water_point.geojson
+ogr2ogr -t_srs EPSG:%user_crs% reproj_water_point.geojson water_point.geojson
 
 :: supermarkets 
-ogr2ogr -t_srs EPSG:32632 reproj_super.geojson supermarkets.geojson
+ogr2ogr -t_srs EPSG:%user_crs% reproj_super.geojson supermarkets.geojson
 
 :: shelters
-ogr2ogr -t_srs EPSG:32632 reproj_shelters.geojson shelters.geojson
+ogr2ogr -t_srs EPSG:%user_crs% reproj_shelters.geojson shelters.geojson
 
 :: sanitary_facilities 
-ogr2ogr -t_srs EPSG:32632 reproj_sani.geojson sanitary_facilities.geojson
+ogr2ogr -t_srs EPSG:%user_crs% reproj_sani.geojson sanitary_facilities.geojson
 
 :: electricity 
 ogr2ogr -t_srs EPSG:32632 reproj_elec.geojson electricity.geojson
 
 :: trainstations 
-ogr2ogr -t_srs EPSG:32632 reproj_train.geojson trainstations.geojson
+ogr2ogr -t_srs EPSG:%user_crs% reproj_train.geojson trainstations.geojson
 
 :: rasterize all layers
 gdal_rasterize -burn 1.0 -tr 10.0 10.0 -ot Float32 -of GTiff reproj_user_area.geojson rast_user_area.tif
